34fbeadbcefedcf3693ce1beea865c71ff7d7d67
zakirovrj <rjzakirov@gmail.com>
2020-08-31 07:08:55 +0200

    JS LEsson_1 - examples, part of lecture

---
+++ 2_quarter/JS_basic/Lesson_1/example_5_2.js
@@ -0,0 +1,12 @@
+'use strict';
+alert('Hi, everybody!');
+console.log('hello, world');
+// console.error('hello, world');
+console.warn('hello, world');
+// console.table(['one', 'two', 'three']);
+
+// one row comment which is commented
+
+/*
+multi row comment
+ */
