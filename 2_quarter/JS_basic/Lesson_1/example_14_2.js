bc6e0df27d1488598dc45df9718fab2f883374ad
R F <R@gmail.com>
2020-09-02 20:43:09 +0200

    JS course - matherials from lesson 1

---
+++ 2_quarter/JS_basic/Lesson_1/example_14_2.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Input
+let name = prompt('Enter you name: ');
+
+//Output
+alert(name);
+
+
+//Confirm
+let answer = confirm('Accept your agreement with rules on our website');
+//Output in console
+console.log(answer);
---
+++ 2_quarter/JS_basic/Lesson_1/example_15_2.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let value = 25;
+value += 10;
+console.log(value); //35
+
+let value2 = 4;
+value2 *= 2;
+console.log(value2); //8
+
+let value3 = 14;
+value3 /= 7;
+console.log(value3); //2
+
+let value4 = 11;
+value4 -= 6;
+console.log(value4); //5
+
+let value5 = 'hello';
+value5 += 'world';
+console.log(value5); //5
